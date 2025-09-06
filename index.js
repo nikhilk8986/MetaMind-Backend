0c4901d4f9202be7e5b7350b3795b54f2d63b112
Nikhil <nikhilraj8986@gmail.com>
2025-09-06 19:31:38 +0530

    commited

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const userRoutes=require('./routes/userRoutes');
 // Environment variables with fallbacks
 const PORT = process.env.PORT || 3000;
 const FRONTEND_URL = process.env.FRONTEND_URL;
-const MONGO_URI = "mongodb://localhost:27017/metamind";
+const MONGO_URI = "mongodb+srv://sayan2003dev:admin@cluster0.fuvcpma.mongodb.net/metamind";
 
 app.use(express.json());
 
